1c39938e2edf707b05afb218b254d7227edf4300
Brian Collet <briancollet@users.noreply.github.com>
2023-01-31 16:26:59 -0800

    Array.diff Updated CodeWars code

--- 6kyu/array-diff.js
+++ 6kyu/array-diff.js
@@ -3,8 +3,14 @@ function arrayDiff(a, b) {
     return a.filter(v => b.indexOf(v) < 0)
 }
 
-// Full Codewars mode
+// Full CodeWars mode
 let arrayDiffCodewarsMode = (a, b) => a.filter(v => b.indexOf(v) < 0)
+
+// Updated CodeWars mode
+let arrayDiffCodewarsModeUpdated = (a, b) => a.filter(v => b.indexOf(v) == -1)
+
+// or
+let arrayDiffCodewarsModeUpdatedTwo = (a, b) => a.filter(v => !b.includes(v))
 
 console.log(arrayDiff([1,2], [1]))
 console.log(arrayDiffCodewarsMode([1,2], [1]))
